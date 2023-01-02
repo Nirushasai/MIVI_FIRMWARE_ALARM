fa755b9cd9dd87b97a6061e77a50b598ef970a3b
Nirushasai <116872199+Nirushasai@users.noreply.github.com>
2023-01-02 19:01:23 +0530

    Add files via upload

---
+++ rtc.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "alarm.h"
+#include "second.h"
+#include "utc.h"
+#include<stdbool.h>
+
+void do_rtc_alarm(void)
+
+{
+
+    printf("do_rtc_alarm\n");
+
+    /*if (RtkWristbandSys.flag_field.power_off_flag == false)
+
+    {
+
+        wristband_system_interact(INTERACT_ALARM, 0, NULL);
+
+    }
+
+    L1_send_event(L1SEND_RETURN_ALARM_EVENT, NULL);
+*/
+}
+
